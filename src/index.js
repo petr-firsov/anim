7cdace6b60ec49e3c4c79aab8c6fc03d1f13e9e1
petr-firsov <125796227+petr-firsov@users.noreply.github.com>
2024-11-25 12:19:38 +0300

    Финал

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import './index.css';
 
-const textbox = document.querySelector('.textbox');
+const block = document.querySelector('.block');
 const conllapseBtn = document.querySelector('.collapse-btn');
 
 conllapseBtn.addEventListener('click', () => {
-    textbox.classList.toggle('visible');
+    if (block.classList.contains('showing-block')) {
+        block.classList.replace('showing-block', 'hiding-block');
+    } else if (block.classList.contains('hiding-block')) {
+        block.classList.replace('hiding-block', 'showing-block');
+    } else {
+        block.classList.add('showing-block');
+    }
 });
+
+/**
+ * Если класса нет, то добавить showing-block
+ * если есть showing-block, то убрать и добавить hiding-block.
+ * Есть есть hiding-block, то удаляем и добавляем showing-block.
+ */
